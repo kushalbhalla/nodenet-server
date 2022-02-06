4f543d8747357de2c4592fa2481e7fce1a9eaaa4
kush <kushalbhalla>
2022-02-06 19:03:06 +0530

    updated

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
